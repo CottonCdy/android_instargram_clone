954657b1492fd53c38fce98cf485378efe45f551
cotton <taste397@naver.com>
2022-08-25 22:51:29 +0900

    commit

--- app/src/main/java/com/example/instargramclone/InstarJoin.kt
+++ app/src/main/java/com/example/instargramclone/InstarJoin.kt
@@ -3,7 +3,7 @@ package com.example.instargramclone
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class InstarJoin : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
